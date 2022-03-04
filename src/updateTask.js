edebbae17a5bd3bdcd79523774813536ab51725f
Luis Gonzales <luis.gonzales35@unmsm.edu.pe>
2022-03-03 19:14:36 -0500

    Subida de proyecto

---
+++ src/updateTask.js
@@ -0,0 +1,33 @@
+const uuid = require("uuid");
+const AWS = require("aws-sdk");
+
+const updateTask = async (event) => {
+  const dynamodb = new AWS.DynamoDB.DocumentClient();
+  const { id } = event.pathParameters;
+
+  const { title, description } = JSON.parse(event.body);
+
+  await dynamodb
+    .update({
+      TableName: "TaskTable",
+      Key: { id },
+      UpdateExpression: "set title = :title, description = :description",
+      ExpressionAttributeValues: {
+        ":title": title,
+        ":description": description,
+      },
+      ReturnValues: "ALL_NEW",
+    })
+    .promise();
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "Tarea actualizada",
+    }),
+  };
+};
+
+module.exports = {
+  updateTask,
+};
